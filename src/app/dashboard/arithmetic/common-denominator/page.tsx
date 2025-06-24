04c90184adefe2b00531b8bbc0a792c16c5ac331
DANH PHI LONG <longgggwww@gmail.com>
2025-06-24 16:13:32 +0700

    fix: update form

--- src/app/dashboard/arithmetic/common-denominator/page.tsx
+++ src/app/dashboard/arithmetic/common-denominator/page.tsx
@@ -1,11 +1,11 @@
 import { CONFIG } from 'src/global-config';
 
-import { BlankView } from 'src/sections/blank/view';
+import { CommonDenominatorView } from 'src/sections/arithmetic/common-denominator-view';
 
 // ----------------------------------------------------------------------
 
 export const metadata = { title: `Mẫu số chung - ${CONFIG.appName}` };
 
 export default function Page() {
-  return <BlankView title="Mẫu số chung" />;
+  return <CommonDenominatorView />;
 }
