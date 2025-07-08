fff5dd2265828feebba96cbeaf161bad961d1489
DANH PHI LONG <longgggwww@gmail.com>
2025-07-08 08:10:25 +0700

    refactor: remove unused imports and help link from AuthSplitLayout

--- src/layouts/auth-split/layout.tsx
+++ src/layouts/auth-split/layout.tsx
@@ -6,10 +6,6 @@ import { merge } from 'es-toolkit';
 
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-
-import { RouterLink } from 'src/routes/components';
-import { paths } from 'src/routes/paths';
 
 
 import { Logo } from 'src/components/logo';
@@ -67,16 +63,6 @@ export function AuthSplitLayout({
       ),
       rightArea: (
         <Box sx={{ display: 'flex', alignItems: 'center', gap: { xs: 1, sm: 1.5 } }}>
-          {/** @slot Help link */}
-          <Link
-            href={paths.faqs}
-            component={RouterLink}
-            color="inherit"
-            sx={{ typography: 'subtitle2' }}
-          >
-            Need help?
-          </Link>
-
           {/** @slot Settings button */}
           <SettingsButton />
         </Box>
