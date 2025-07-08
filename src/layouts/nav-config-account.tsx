bd9e635daea683abd0466b1aa4930aa302526458
DANH PHI LONG <longgggwww@gmail.com>
2025-07-08 10:48:41 +0700

    refactor: update icon imports in account drawer and nav config, removing unused icons

--- src/layouts/nav-config-account.tsx
+++ src/layouts/nav-config-account.tsx
@@ -1,15 +1,15 @@
-import { Iconify } from 'src/components/iconify';
+import { HomeIcon, SettingsIcon, UserIcon } from 'src/assets/icons';
 
 import type { AccountDrawerProps } from './components/account-drawer';
 
 // ----------------------------------------------------------------------
 
 export const _account: AccountDrawerProps['data'] = [
-  { label: 'Home', href: '/', icon: <Iconify icon="solar:home-angle-bold-duotone" /> },
+  { label: 'Home', href: '/', icon: <HomeIcon /> },
   {
     label: 'Profile',
     href: '/dashboard/user/profile',
-    icon: <Iconify icon="solar:user-rounded-bold" />,
+    icon: <UserIcon />,
   },
-  { label: 'Account Settings', href: '/dashboard/user/account', icon: <Iconify icon="solar:settings-bold-duotone" /> },
+  { label: 'Account Settings', href: '/dashboard/user/account', icon: <SettingsIcon /> },
 ];
