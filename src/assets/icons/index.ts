b698efdde121d8306d6643bb8a6f916064aee6ac
DANH PHI LONG <longgggwww@gmail.com>
2025-07-09 18:32:03 +0700

    Remove unused icon components and update icon index export

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -9,13 +9,4 @@ export { ResetIcon } from './reset-icon';
 
 export { SettingsIcon } from './settings-icon';
 
-export { default as SentIcon } from './sent-icon';
-
 export { FullscreenIcon } from './fullscreen-icon';
-
-export { default as PasswordIcon } from './password-icon';
-export { default as PlanFreeIcon } from './plan-free-icon';
-export { default as EmailInboxIcon } from './email-inbox-icon';
-export { default as PlanStarterIcon } from './plan-starter-icon';
-export { default as PlanPremiumIcon } from './plan-premium-icon';
-export { default as NewPasswordIcon } from './new-password-icon';
