ea245cc5e513a8c37d28bd69b70a3bb2a8263f42
DANH PHI LONG <longgggwww@gmail.com>
2025-07-09 08:26:51 +0700

    refactor: Reorganize imports and remove unused paths in layout and routing files

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -8,8 +8,6 @@ const ROOTS = {
 // ----------------------------------------------------------------------
 
 export const paths = {
-  faqs: '/faqs',
-  minimalStore: 'https://mui.com/store/items/minimal-dashboard/',
   // AUTH
   auth: {
     jwt: {
